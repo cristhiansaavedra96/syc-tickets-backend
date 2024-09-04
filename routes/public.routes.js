7a55281d48d6b52f9438493a5894acde1b821de5
Cristhian Saavedra <45241687+cristhiansaavedra96@users.noreply.github.com>
2024-09-04 16:25:01 -0300

    test sharon

--- routes/public.routes.js
+++ routes/public.routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import userController from '../controllers/user.controller.js';
+import { recipes } from '../services/recipes.js';
 
 const router = express.Router();
 
@@ -8,5 +9,11 @@ router.post('/password/recover', userController.passwordRecover)
 router.put('/password/recover', userController.passwordUpdateByRecoverCode)
 router.post('/validate/', userController.validate)
 router.get('/health-check', (req, res) => res.send('OK'))
+
+//get recipes from services/recipes.json
+
+router.get('/recipes', async (req, res) => {
+    res.send(recipes)
+})
 
 export default router;
